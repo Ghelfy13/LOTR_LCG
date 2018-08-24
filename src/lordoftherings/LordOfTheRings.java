1889fbab85ff7ccb628ccd7d437477665dd7b575
Amanda Harris <amanda.victoria.harris@gmail.com>
2018-08-23 17:09:35 -0700

    Created PlayerCard database to help with readability

--- src/lordoftherings/LordOfTheRings.java
+++ src/lordoftherings/LordOfTheRings.java
@@ -23,6 +23,7 @@ import lordoftherings.transaction_managers.GameManager;
 import lordoftherings.characters.Enemy;
 import lordoftherings.boardcomponents.Location;
 import lordoftherings.cardmodel.EventCardModel;
+import lordoftherings.cards.AllyCard;
 import lordoftherings.deckcomponents.ExpansionName;
 import lordoftherings.effects.DiscardToAddModifiersEffect;
 import lordoftherings.effects.DiscardToDrawEffect;
@@ -76,97 +77,6 @@ public class LordOfTheRings {
             2,
             1,
             "");
-        HealEffect healHero = new HealEffect(2);
-        ArrayList<Effect> list = new ArrayList<>();
-        list.add(healHero);
-        
-        ReadyAllyToDiscardCardEffect discardToReady = new ReadyAllyToDiscardCardEffect();
-        ArrayList<Effect> list2 = new ArrayList<>();
-        list2.add(discardToReady);
-        
-        HealPlayersHerosAndDiscardCardEffect healPlayersHeros = new HealPlayersHerosAndDiscardCardEffect();
-        ArrayList<Effect> list3 = new ArrayList<>();
-        list3.add(healPlayersHeros);
-        
-        DiscardToDrawEffect discardToDraw = new DiscardToDrawEffect(3);
-        ArrayList<Effect> list4 = new ArrayList<>();
-        list4.add(discardToDraw);
-        
-        ExhaustToDrawEffect exhaustToDraw = new ExhaustToDrawEffect(2);
-        ArrayList<Effect> list5 = new ArrayList<>();
-        list5.add(exhaustToDraw);
-        
-        ExhaustToRaiseWillPowerEffect exhaustToRaiseWillPower = 
-                new ExhaustToRaiseWillPowerEffect(1, new Modifier(1, TypeOfModifier.WILL_POWER, LifeSpanOfModifier.ENDOFPHASE));
-        ArrayList<Effect> list6 = new ArrayList<>();
-        list6.add(exhaustToRaiseWillPower);
-        
-        ExhaustToDrawEffect exhaustToDrawGleowine = new ExhaustToDrawEffect(1);
-        ArrayList<Effect> list7 = new ArrayList<>();
-        list7.add(exhaustToDrawGleowine);
-        
-        Modifier attack = new Modifier(1, TypeOfModifier.ATTACK, LifeSpanOfModifier.ENDOFPHASE);
-        Modifier defense = new Modifier(1, TypeOfModifier.DEFENSE, LifeSpanOfModifier.ENDOFPHASE);
-        ArrayList<Modifier> mods = new ArrayList<>();
-        mods.add(attack);
-        mods.add(defense);
-        DiscardToAddModifiersEffect playBladeMastery = new DiscardToAddModifiersEffect(mods);
-        ArrayList<Effect> list8 = new ArrayList<>();
-        list8.add(playBladeMastery);
-        
-        DiscardToExhaustAndReadyEffect exhaustReadyEffect = new DiscardToExhaustAndReadyEffect();
-        ArrayList<Effect> list9 = new ArrayList<>();
-        list9.add(exhaustReadyEffect);
-        
-        Identification CommonCauseID = new Identification(ExpansionName.CORE,21);
-        EventCardModel commonCause = new EventCardModel(
-            "Common Cause",
-            SphereOfInfluence.LEADERSHIP,
-            PlayerCardType.EVENT,
-            new String []{""},
-            CommonCauseID,
-            0,
-            "Exhaust 1 hero you control to choose and ready a different hero.",
-            list9);
-        
-        Identification BladeMasteryID = new Identification(ExpansionName.CORE, 32);
-        EventCardModel bladeMastery = new EventCardModel(
-            "Blade Mastery",
-            SphereOfInfluence.LORE,
-            PlayerCardType.EVENT,
-            new String []{""},
-            BladeMasteryID,
-            1,
-            "Choose a character.  That character gets +1 attack and +1 defend this round",
-            list8);
-                
-        Identification EverVigilantID = new Identification(ExpansionName.CORE, 20);
-        EventCardModel everVigilant = new EventCardModel(
-            "Ever Vigilant", 
-            SphereOfInfluence.LEADERSHIP,
-            PlayerCardType.EVENT,
-            new String[]{""},
-            EverVigilantID,
-            1,
-            "Choose and ready 1 ally card and discard card",
-            list2) ;
-        
-        
-        Identification BeravorID = new Identification(ExpansionName.CORE, 12);
-        HeroCardModel beravor = new HeroCardModel(
-            "Beravor",
-            SphereOfInfluence.LORE,
-            PlayerCardType.HERO,
-            new String [] {"Dunedain", "Ranger"},
-            BeravorID,
-            2, 
-            2,
-            2,
-            4,
-            10,
-            4,
-            "",
-            list5);
         
         Identification AragornID = new Identification(ExpansionName.CORE, 1);
         HeroCardModel first = new HeroCardModel(
@@ -183,75 +93,6 @@ public class LordOfTheRings {
                 0,
                 "",
                 new ArrayList<>());
-        
-        
-        Identification loriensWealthID = new Identification(ExpansionName.CORE, 64);
-        EventCardModel loriensWealth = new EventCardModel(
-            "Lorien's Wealth",
-            SphereOfInfluence.LORE,
-            PlayerCardType.EVENT,
-            new String[]{""},
-            loriensWealthID,
-            3,
-            "Choose a player.  That player draws 3 cards",
-            list4);
-        
-        Identification BeornsHospitalityID = new Identification(ExpansionName.CORE, 68);
-        EventCardModel beornsHospitality = new EventCardModel(
-            "Beorn's Hospitality", 
-            SphereOfInfluence.LORE,
-            PlayerCardType.EVENT,
-            new String[]{""},
-            BeornsHospitalityID,
-            5,
-            "Choose a player, and heal all damage on each hero controlled by that player.",
-            list3);
-        
-        Identification DaughterID = new Identification(ExpansionName.CORE, 23);
-        AllyCardModel second = new AllyCardModel(
-          "Daughter of Nimrodel",
-          SphereOfInfluence.LORE,
-          PlayerCardType.ALLY,
-          new String[]{"Silvan"},
-          DaughterID,
-          1,
-          0,
-          0,
-          1,
-          3,
-          "Exhaust Daughter of Nimrodel to heal up to 2 damage on any 1 hero",
-          list);
-        
-        Identification GleowineID = new Identification(ExpansionName.CORE, 62);
-        AllyCardModel gleowine = new AllyCardModel(
-            "Gleowine",
-            SphereOfInfluence.LORE,
-            PlayerCardType.ALLY,
-            new String[]{"Minstrel", "Rohan"},
-            GleowineID,
-            2,
-            1,
-            0,
-            0,
-            2,
-            "Exhaust Gleowine to choose a player.  That player draws 1 card.",
-            list7);
-        
-        Identification FaramirID = new Identification(ExpansionName.CORE, 14);
-        AllyCardModel faramir = new AllyCardModel(
-            "Faramir",
-            SphereOfInfluence.LEADERSHIP,
-            PlayerCardType.ALLY,
-            new String [] {"Gondor", "Noble", "Ranger"},
-            FaramirID,
-            4,
-            2,
-            1,
-            2,
-            3,
-            "Exhaust Faramir to choose a player.  Each character controlled "
-                    + "by the player gets +1 Willpower until the end of the phase.",
-            list6);
         
         Identification QuestID = new Identification(ExpansionName.CORE, 122);
         QuestCardModel firstQuest = new QuestCardModel(
@@ -274,18 +115,20 @@ public class LordOfTheRings {
                 2);
         
         DeckBuild mockDeck = new DeckBuild();
-        mockDeck.add(commonCause, 3);
-        mockDeck.add(bladeMastery, 0);
+        Identification CommonCauseID = new Identification(ExpansionName.CORE,21);
+        mockDeck.add(PlayerCardDatabase.get(CommonCauseID), 3);
+       /*mockDeck.add(bladeMastery, 0);
         mockDeck.add(everVigilant, 0);
         mockDeck.add(second, 0);
         mockDeck.add(beornsHospitality, 0);
         mockDeck.add(faramir,0);
         mockDeck.add(gleowine, 0);
-        mockDeck.add(loriensWealth, 0);
+        mockDeck.add(loriensWealth, 0);*/
         PlayerDeckBuild mockBuild = new PlayerDeckBuild(mockDeck);
         mockBuild.addHero(first);
         mockBuild.addHero(first);
-        mockBuild.addHero(beravor);
+        Identification BeravorID = new Identification(ExpansionName.CORE, 12);
+        mockBuild.addHero((HeroCardModel)PlayerCardDatabase.get(BeravorID));
         PlayerDeckBuild[] mockBuildArray = new PlayerDeckBuild[1];
         mockBuildArray[0] = mockBuild;
         
@@ -301,7 +144,9 @@ public class LordOfTheRings {
         GameManager manager = new GameManager(mockBuildArray, enemyBuild);
         Board newBoard = manager.getBoard();
         newBoard.getPlayerZoneAt(0).addResourcesToHero(0, 6);
-        newBoard.getPlayerZoneAt(0).getCharZone().addAllyToAllyZone(second.createCard());
+        Identification DaughterID = new Identification(ExpansionName.CORE, 23);
+        newBoard.getPlayerZoneAt(0).getCharZone().
+                addAllyToAllyZone((AllyCard)PlayerCardDatabase.get(DaughterID).createCard());
         newBoard.getEncounterZone().addNumOfCardsToDeck(firstEnemy, 5);
         newBoard.getEncounterZone().addNumOfCardsToDeck(firstLocation, 3);
         newBoard.getPlayerZoneAt(0).getEngagementArea().addEnemy(enemy);
---
+++ src/lordoftherings/PlayerCardDatabase.java
@@ -0,0 +1,203 @@
+//Copyright Amanda V. Harris 2018-present. All Rights Reserved.
+
+package lordoftherings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import lordoftherings.cardmodel.AllyCardModel;
+import lordoftherings.cardmodel.EventCardModel;
+import lordoftherings.cardmodel.HeroCardModel;
+import lordoftherings.cardmodel.PlayerCardModel;
+import lordoftherings.deckcomponents.ExpansionName;
+import lordoftherings.deckcomponents.SphereOfInfluence;
+import lordoftherings.effects.DiscardToAddModifiersEffect;
+import lordoftherings.effects.DiscardToDrawEffect;
+import lordoftherings.effects.DiscardToExhaustAndReadyEffect;
+import lordoftherings.effects.Effect;
+import lordoftherings.effects.ExhaustToDrawEffect;
+import lordoftherings.effects.ExhaustToRaiseWillPowerEffect;
+import lordoftherings.effects.HealEffect;
+import lordoftherings.effects.HealPlayersHerosAndDiscardCardEffect;
+import lordoftherings.effects.ReadyAllyToDiscardCardEffect;
+import lordoftherings.modifiers.LifeSpanOfModifier;
+import lordoftherings.modifiers.Modifier;
+import lordoftherings.modifiers.TypeOfModifier;
+
+/**
+ *
+ * @author Amanda
+ */
+public class PlayerCardDatabase {
+    
+    private static HashMap<Identification, PlayerCardModel> cardMap = null;
+    
+    public static PlayerCardModel get(Identification id) {
+        initializeCardMap();
+        return cardMap.get(id);
+    }
+    
+    public static void initializeCardMap() {
+        if(cardMap != null) return;
+        cardMap = new HashMap<>();
+        
+        HealEffect healHero = new HealEffect(2);
+        ArrayList<Effect> list = new ArrayList<>();
+        list.add(healHero);
+        Identification DaughterID = new Identification(ExpansionName.CORE, 23);
+        AllyCardModel second = new AllyCardModel(
+          "Daughter of Nimrodel",
+          SphereOfInfluence.LORE,
+          PlayerCardType.ALLY,
+          new String[]{"Silvan"},
+          DaughterID,
+          1,
+          0,
+          0,
+          1,
+          3,
+          "Exhaust Daughter of Nimrodel to heal up to 2 damage on any 1 hero",
+          list);
+        cardMap.put(DaughterID, second);
+        
+        ReadyAllyToDiscardCardEffect discardToReady = new ReadyAllyToDiscardCardEffect();
+        ArrayList<Effect> list2 = new ArrayList<>();
+        list2.add(discardToReady);
+        Identification EverVigilantID = new Identification(ExpansionName.CORE, 20);
+        EventCardModel everVigilant = new EventCardModel(
+            "Ever Vigilant", 
+            SphereOfInfluence.LEADERSHIP,
+            PlayerCardType.EVENT,
+            new String[]{""},
+            EverVigilantID,
+            1,
+            "Choose and ready 1 ally card and discard card",
+            list2);
+        cardMap.put(EverVigilantID, everVigilant);
+        
+        HealPlayersHerosAndDiscardCardEffect healPlayersHeros = new HealPlayersHerosAndDiscardCardEffect();
+        ArrayList<Effect> list3 = new ArrayList<>();
+        list3.add(healPlayersHeros);
+        Identification BeornsHospitalityID = new Identification(ExpansionName.CORE, 68);
+        EventCardModel beornsHospitality = new EventCardModel(
+            "Beorn's Hospitality", 
+            SphereOfInfluence.LORE,
+            PlayerCardType.EVENT,
+            new String[]{""},
+            BeornsHospitalityID,
+            5,
+            "Choose a player, and heal all damage on each hero controlled by that player.",
+            list3);
+        cardMap.put(BeornsHospitalityID, beornsHospitality);
+        
+        DiscardToDrawEffect discardToDraw = new DiscardToDrawEffect(3);
+        ArrayList<Effect> list4 = new ArrayList<>();
+        list4.add(discardToDraw);
+        Identification loriensWealthID = new Identification(ExpansionName.CORE, 64);
+        EventCardModel loriensWealth = new EventCardModel(
+            "Lorien's Wealth",
+            SphereOfInfluence.LORE,
+            PlayerCardType.EVENT,
+            new String[]{""},
+            loriensWealthID,
+            3,
+            "Choose a player.  That player draws 3 cards",
+            list4);
+        cardMap.put(loriensWealthID, loriensWealth);
+        
+        ExhaustToDrawEffect exhaustToDraw = new ExhaustToDrawEffect(2);
+        ArrayList<Effect> list5 = new ArrayList<>();
+        list5.add(exhaustToDraw);
+        Identification BeravorID = new Identification(ExpansionName.CORE, 12);
+        HeroCardModel beravor = new HeroCardModel(
+            "Beravor",
+            SphereOfInfluence.LORE,
+            PlayerCardType.HERO,
+            new String [] {"Dunedain", "Ranger"},
+            BeravorID,
+            2, 
+            2,
+            2,
+            4,
+            10,
+            4,
+            "",
+            list5);
+        cardMap.put(BeravorID, beravor);
+        
+        ExhaustToRaiseWillPowerEffect exhaustToRaiseWillPower = 
+                new ExhaustToRaiseWillPowerEffect(1, new Modifier(1, TypeOfModifier.WILL_POWER, LifeSpanOfModifier.ENDOFPHASE));
+        ArrayList<Effect> list6 = new ArrayList<>();
+        list6.add(exhaustToRaiseWillPower);
+        Identification FaramirID = new Identification(ExpansionName.CORE, 14);
+        AllyCardModel faramir = new AllyCardModel(
+            "Faramir",
+            SphereOfInfluence.LEADERSHIP,
+            PlayerCardType.ALLY,
+            new String [] {"Gondor", "Noble", "Ranger"},
+            FaramirID,
+            4,
+            2,
+            1,
+            2,
+            3,
+            "Exhaust Faramir to choose a player.  Each character controlled "
+                    + "by the player gets +1 Willpower until the end of the phase.",
+            list6);
+        cardMap.put(FaramirID, faramir);
+        
+        ExhaustToDrawEffect exhaustToDrawGleowine = new ExhaustToDrawEffect(1);
+        ArrayList<Effect> list7 = new ArrayList<>();
+        list7.add(exhaustToDrawGleowine);
+        Identification GleowineID = new Identification(ExpansionName.CORE, 62);
+        AllyCardModel gleowine = new AllyCardModel(
+            "Gleowine",
+            SphereOfInfluence.LORE,
+            PlayerCardType.ALLY,
+            new String[]{"Minstrel", "Rohan"},
+            GleowineID,
+            2,
+            1,
+            0,
+            0,
+            2,
+            "Exhaust Gleowine to choose a player.  That player draws 1 card.",
+            list7);
+        cardMap.put(GleowineID, gleowine);
+        
+        Modifier attack = new Modifier(1, TypeOfModifier.ATTACK, LifeSpanOfModifier.ENDOFPHASE);
+        Modifier defense = new Modifier(1, TypeOfModifier.DEFENSE, LifeSpanOfModifier.ENDOFPHASE);
+        ArrayList<Modifier> mods = new ArrayList<>();
+        mods.add(attack);
+        mods.add(defense);
+        DiscardToAddModifiersEffect playBladeMastery = new DiscardToAddModifiersEffect(mods);
+        ArrayList<Effect> list8 = new ArrayList<>();
+        list8.add(playBladeMastery);
+        Identification BladeMasteryID = new Identification(ExpansionName.CORE, 32);
+        EventCardModel bladeMastery = new EventCardModel(
+            "Blade Mastery",
+            SphereOfInfluence.LORE,
+            PlayerCardType.EVENT,
+            new String []{""},
+            BladeMasteryID,
+            1,
+            "Choose a character.  That character gets +1 attack and +1 defend this round",
+            list8);
+        cardMap.put(BladeMasteryID, bladeMastery);
+        
+        DiscardToExhaustAndReadyEffect exhaustReadyEffect = new DiscardToExhaustAndReadyEffect();
+        ArrayList<Effect> list9 = new ArrayList<>();
+        list9.add(exhaustReadyEffect);
+        Identification CommonCauseID = new Identification(ExpansionName.CORE,21);
+        EventCardModel commonCause = new EventCardModel(
+            "Common Cause",
+            SphereOfInfluence.LEADERSHIP,
+            PlayerCardType.EVENT,
+            new String []{""},
+            CommonCauseID,
+            0,
+            "Exhaust 1 hero you control to choose and ready a different hero.",
+            list9);
+        cardMap.put(CommonCauseID, commonCause);
+    }
+    
+}
